e3c8168f5d64f451dee60fcef88197bd54cca9f2
Caty Gomes Fernandes <catygomes@outlook.com>
2020-06-22 13:38:44 +0200

    Handle multi-parts episodes

--- scrape_episode_urls.py
+++ scrape_episode_urls.py
@@ -9,6 +9,8 @@ import re
 import csv
 from bs4 import BeautifulSoup
 
+from collections import defaultdict
+
 BASE_URL = "https://transcripts.fandom.com"
 WIKI_URL = "https://transcripts.fandom.com/wiki/Avatar:_The_Last_Airbender"
 OUTPUT_DIR = "raw_data"
@@ -27,14 +29,14 @@ def fetch_html_content(url=WIKI_URL):
     """Fetches the HTML content of the given URL. Returns None if the status code is not 200."""
     r = requests.get(url)
     if r.status_code != 200:
-        return None
+        sys.stderr.write("ERROR. Couldn't fetch the main page.\n")
+        sys.exit(2)
     return r.content
 
 
 def extract_transcript_urls(page_content):
     """Parse the HTML content and find all links to the transcript pages"""
-    urls = []
-
+    urls_dict = defaultdict(list)
     soup = BeautifulSoup(page_content, features="html.parser")
 
     for book_number, book_id in enumerate(BOOK_IDS, 1):
@@ -42,26 +44,41 @@ def extract_transcript_urls(page_content):
         for table_row in book_content.find_all("tr"):
             episode_number = int(table_row.find("th").text.strip('\n '))
             link = table_row.find("a")
-            urls.append((book_number, episode_number, link.text, link.get("href")))
-    return urls
+            urls_dict[link.get("href")].append((book_number, episode_number, link.text))
+    return urls_dict
 
 
-def extract_transcripts_to_files(urls):
+def extract_transcripts_to_files(urls_dict):
     """For each url, fetch the page and extract the text into a file"""
-    for book_number, episode_number, episode_name, url in urls:
+    for url, episodes in urls_dict.items():
         content = fetch_html_content(BASE_URL + url)
         if not content:
             sys.stderr.write("ERROR. Couldn't fetch the transcript from {}.\n".format(url))
             sys.exit(2)
         soup = BeautifulSoup(content, features="html.parser")
+        main_body = soup.find(id="WikiaArticle")
 
-        output = []
-        for character in soup.find(id="WikiaArticle").find_all("th"):
-            if character.next_sibling:
-                line = character.next_sibling.text.strip("\n")
-                line = re.sub(r"\[.*\] ?", "", line)
-                if line.strip():
-                    output.append((character.text.strip("\n"), line))
+        if len(episodes) == 1:
+            create_episode_file(main_body, episodes[0])
+        else:
+            for part_number, episode in enumerate(episodes, 1):
+                part_title = main_body.find("span", text=re.compile('^Part {}'.format(part_number))).parent
+                part_body = part_title.next_sibling.next_sibling
+                create_episode_file(part_body, episode)
+
+
+def create_episode_file(table_content, episode_info):
+    """Create and write the transcriptions for one episode to a file"""
+    book_number, episode_number, episode_name = episode_info
+    output = []
+
+    for character in table_content.find_all("th"):
+        if character.next_sibling:
+            line = character.next_sibling.text.strip("\n")
+            while '[' in line:
+                line = re.sub(r"\[[^\]]*\] ?", "", line)
+            if line.strip():
+                output.append((character.text.strip("\n"), line))
 
         filename = os.path.join(
             OUTPUT_DIR,
@@ -82,10 +99,6 @@ if __name__ == "__main__":
     create_output_folder()
 
     html_content = fetch_html_content()
-
-    if not html_content:
-        sys.stderr.write("ERROR. Couldn't fetch the main page.\n")
-        sys.exit(2)
 
     print("Copying all transcripts to raw_data/ ...")
 
