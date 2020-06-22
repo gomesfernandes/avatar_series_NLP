f09e6a6367a58257843f179fd73980458662e490
Caty Gomes Fernandes <catygomes@outlook.com>
2020-06-22 15:09:23 +0200

    Break down bigger functions

--- scrape_episode_urls.py
+++ scrape_episode_urls.py
@@ -44,7 +44,7 @@ def fetch_html_content(url=WIKI_URL):
 
 
 def extract_transcript_urls(page_content):
-    """Parse the HTML content and find all links to the pages containing the actual transcriptions.
+    """Parse the HTML content and find all links to the pages containing the actual transcripts.
     The URLs are relative to the base URL (transcripts.fandom.com).
     Episodes that were split into parts point to the same URL.
 
@@ -65,6 +65,30 @@ def extract_transcript_urls(page_content):
             urls_dict[link.get("href")].append((book_number, episode_number, link.text))
 
     return urls_dict
+
+
+def format_filename(output_dir, book_number, episode_number, episode_name):
+    """Create a filename with the following format: output_dir/transcript_{book_number}_{episode_number}_{title}.csv
+
+    Title corresponds to the episode name without commas, and with underscores instead of spaces.
+
+    Args:
+        output_dir (str): the path to output directory
+        book_number (int): the season number
+        episode_number (int): the episode number relative to the season
+        episode_name (str): the episode's name
+
+    Returns:
+        The formatted filename
+    """
+    return os.path.join(
+        output_dir,
+        OUTPUT_FILENAME.format(
+            book_number,
+            episode_number,
+            episode_name.replace(',', '').replace(' ', '_')
+        )
+    )
 
 
 def extract_transcripts_to_files(output_dir, urls_dict):
@@ -91,27 +115,32 @@ def extract_transcripts_to_files(output_dir, urls_dict):
         main_body = soup.find(id="WikiaArticle")
 
         if len(episodes) == 1:
-            create_transcript_for_episode(output_dir, main_body, episodes[0])
+            book_number, episode_number, episode_name = episodes[0]
+            transcripts = create_transcript_for_episode(main_body)
+            filename = format_filename(output_dir, book_number, episode_number, episode_name)
+            write_transcript_to_csv(filename, transcripts)
         else:
-            for part_number, episode in enumerate(episodes, 1):
+            for part_number, episode_info in enumerate(episodes, 1):
+                book_number, episode_number, episode_name = episode_info
                 part_title = main_body.find("span", text=re.compile('^Part {}'.format(part_number))).parent
                 part_body = part_title.next_sibling.next_sibling
-                create_transcript_for_episode(output_dir, part_body, episode)
+                transcripts = create_transcript_for_episode(part_body)
+                filename = format_filename(output_dir, book_number, episode_number, episode_name)
+                write_transcript_to_csv(filename, transcripts)
 
 
-def create_transcript_for_episode(output_dir, episode_content, episode_info):
-    """Extract and write the transcriptions for one episode to a file.
+def create_transcript_for_episode(episode_content):
+    """Extract the transcript from the given HTML content.
 
-    The output is written to a CSV in the output_dir using the following format for the filename:
-        transcript_{book number}_{episode number}_{title}.csv
+    Only spoken lines are included; any descriptions between brackets ([]) are removed.
 
     Args:
-        output_dir (str): name of the output directory
-        episode_content (bs4.element.Tag): the HTML elements containing the dialog for the given episode
-        episode_info (tuple): containing (book_number, episode_number, episode_name)
+        episode_content (bs4.element.Tag): the HTML content containing the dialog for the given episode
+
+    Returns:
+        A list of (character, dialog) tuples.
     """
-    book_number, episode_number, episode_name = episode_info
-    output = []
+    transcript = []
 
     for character in episode_content.find_all("th"):
         if character.next_sibling:
@@ -119,20 +148,24 @@ def create_transcript_for_episode(output_dir, episode_content, episode_info):
             while '[' in line:
                 line = re.sub(r"\[[^\]]*\] ?", "", line)
             if line.strip():
-                output.append((character.text.strip("\n"), line))
+                transcript.append((character.text.strip("\n"), line))
 
-        filename = os.path.join(
-            output_dir,
-            OUTPUT_FILENAME.format(
-                book_number,
-                episode_number,
-                episode_name.replace(',', '').replace(' ', '_')
-            )
-        )
-        with open(filename, "w", newline="\n", encoding="utf-8") as file:
-            for row in output:
-                writer = csv.writer(file)
-                writer.writerow([row[0], row[1]])
+    return transcript
+
+
+def write_transcript_to_csv(filename, transcript):
+    """Write the transcript to a CSV file with the given filename.
+
+    The transcript is a list of (character, line) tuples: each tuple is one line of the CSV.
+
+    Args:
+        filename (str): name of the CSV output file
+        transcript (list): list of (character, line) tuples
+    """
+    with open(filename, "w", newline="\n", encoding="utf-8") as file:
+        writer = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+        for character, dialog in transcript:
+            writer.writerow([character, dialog])
 
 
 if __name__ == "__main__":
